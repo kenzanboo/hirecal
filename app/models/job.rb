2de5062c95907e03806767c2fcb8496c7be84903
Kenzan Boo <kenzanboo@gmail.com>
2011-03-20 19:52:21 -0700

    added images

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,7 @@
 class Job < ActiveRecord::Base
+  
+    validates :name,  :presence => true
+    validates :pay,  :presence => true
+    validates :description, :presence => true, :length => { :minimum => 5 }
+  
 end
